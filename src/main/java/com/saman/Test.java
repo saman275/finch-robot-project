f662524deed7f710639f01c6364d577ae1fe6253
saman275 <san0143@mymail.lausd.net>
2025-06-03 12:12:24 -0700

    untracked to index

--- src/main/java/com/saman/Test.java
+++ src/main/java/com/saman/Test.java
@@ -1,5 +1,10 @@
 package com.saman;
 
-public class Test {
+public class FinchMoveTurn {
+    public static void main(String [] args) {
 
+        Finch bird = new Finch();
+        bird.setMove("F", 10, 50);
+        bird.disconnect();
+    }
 }
