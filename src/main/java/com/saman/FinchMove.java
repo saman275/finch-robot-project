2385e17a24c9b5220a96dda7318a7ea0bdf2f2f9
saman275 <san0143@mymail.lausd.net>
2025-06-03 12:12:39 -0700

    fafa

---
+++ src/main/java/com/saman/FinchMove.java
@@ -0,0 +1,20 @@
+package com.saman;
+
+import com.birdbrain.Finch;
+
+public class FinchMove {
+    public static void main(String [] args) {
+
+        Finch myfinch = new Finch(); 
+        myfinch.setMove("F", 95, 70);
+        myfinch.setTurn("L", 90, 80);
+
+        
+        
+        
+        
+        
+        
+        bird.disconnect();
+    }
+}
